fda989b8992c1e4cacbb3af69d7e464afca8aa0b
rahulpaul-s <rpaul5820@gmail.com>
2020-09-24 15:31:05 +0530

    Added Week-1 Assignments

---
+++ Ass1_Q2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+  int M, N;
+  scanf("%d%d",&M,&N);
+  
+  if ((M % N) == 1){
+
+    printf("1");
+  }
+  
+  else {
+    
+    printf("0");
+  
+  }
+  
+
+}
---
+++ Ass1_Q3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  
+  int a, b, c;
+  
+  scanf("%d%d%d",&a,&b,&c);
+  
+  if (a > b && b > c) {
+    printf("1");
+  
+  }
+  
+  else if (a < b && b < c) {
+    printf("1");
+  
+  }
+  
+  else {
+    printf("0");
+  
+  }
+
+
+}
